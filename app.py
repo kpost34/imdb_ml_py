c3eb5413585fe15fbf902abc265897a7e2d4df0e
Keith <kpost342005@hotmail.com>
2024-07-08 21:36:19 -0600

    Added raw data to workflow to display additional fields, created inputs and outputs for UI code, updated text, and developed server code for reactive outputs

--- app.py
+++ app.py
@@ -9,21 +9,42 @@ import pandas as pd
 
 
 ## Algorithm function
-from app_fns import get_rec_cosine
+from app_fns import get_movie_info, get_rec_cosine
 
 
 ## Data
+#clean data
 root = '/Users/keithpost/Documents/Python/Python projects/imdb_ml_py/'
 os.chdir(root + 'data')
 file = open('data_final.pkl', 'rb')
 df0 = pickle.load(file)
 
+#original data
+df_orig0 = pd.read_csv('imdb_top_1000.csv')
 
 
-# Generate Cosine Similarity Matrix for app=========================================================
+
+# Data Wrangling====================================================================================
+## Clean original data file
+cols_retain = ['Series_Title', 'Released_Year', 'Genre', 'Director', 'Star1']
+
+df_orig = df_orig0[cols_retain].rename(columns={'Series_Title': 'title',
+                                                'Released_Year': 'year',
+                                                'Star1': 'Starring'})
+
+df_orig['year'] = df_orig['year'].replace('PG', '1995', regex=True)
+
+df_orig['year'] = df_orig['year'].astype(float)
+
+
+## Generate cosine similarity matrix for app
 #cosine similarity matrix
 df = df0.drop(['title', 'year'], axis=1)
 cosine_sim = cosine_similarity(df)
+
+
+## Join clean df and raw df
+df0 = df0.merge(df_orig, on=['title', 'year'])
 
 
 
@@ -32,21 +53,31 @@ app_ui = ui.page_fluid(
   ui.navset_pill(
     ui.nav_panel("App", 
       ui.tags.br(),
-      ui.h2("App content"),
+      ui.h2("Movie Recommender"),
+      
       #inputs
-      ui.input_text("txt_movie", "Select a movie"),
-      ui.input_slider("sld_n_recs", "Number of recommendations", min=1, max=10, value=5),
-      ui.input_checkbox("chk_sim_score", "Similarity score", False),
-      ui.input_checkbox("chk_rank", "IMDB rank", False),
+      ui.input_text("txt_movie", "Enter a movie title"),
+      ui.input_slider("sld_n_recs", "Number of recommendations", min=1, max=8, value=5),
+      ui.input_checkbox("chk_sim_score", "Similarity score?", False),
+      ui.input_checkbox("chk_imdb_rank", "IMDB rank?", False),
       
       #output
+      ui.h2(ui.output_text("txt_sel")),
+      ui.output_data_frame("tab_sel"),
+      
+      ui.tags.br(),
+      
+      ui.h2(ui.output_text("txt_recs")),
       ui.output_data_frame("tab_recs")
     ),
     
     ui.nav_panel("Instructions", 
       ui.tags.br(),
-      ui.h2("Instructions info"),
-      "--add instructions here--"
+      """Please enter the title of a movie in the dialog box. Use the slider to set the number of
+      recommended movies returned. By default, it will return the rank (i.e., strongest to weakest 
+      recommendation out of the total list), title, genre(s), director, and first star. The 
+      similarity score (i.e., from the cosine similarity matrix using engineered features) and
+      IMDB Rank (within the top 1000) are optionally returned using the checkboxes."""
     ),
   
     ui.nav_panel("More info", 
@@ -73,13 +104,13 @@ app_ui = ui.page_fluid(
         """The data were assessed for missingness, which was found for certificate, meta-score, and
         grossed money earned. Little's MCAR test was performed, which was significant suggesting
         that missingness was not completely at random. Certificate was imputed using the most
-        frequent category. Meta-score and grossed money earned were impute using K-nearest neighbors."""
+        frequent category. Meta-score and grossed money earned were imputed using K-nearest neighbors."""
       ),
       
       ui.p(
         """Multicollinearity was assessed for all numerical fields and no pair was found to be highly
-        correlated (spearman rank correlation > 0.9). Rare label encoding was conducted on certificate,
-        director, and star fields (4) by retaining the most common categories and binning the remainder
+        correlated (spearman rank correlation < 0.9 for all comparisons). Rare label encoding was conducted on certificate,
+        director, and all four star fields by retaining the most common categories and binning the remainder
         as "Other". These six features underwent one-hot encoding to generate sets of dummy variables.
         All remaining numerical fields (excluding 0-1 dummy variables, strings, etc.) underwent
         min-max scaling because of non-normal distributions."""
@@ -92,7 +123,7 @@ app_ui = ui.page_fluid(
       ui.p("""A cosine similarity matrix was generated using the cleaned, feature-engineered top 1000
       IMDB movies dataset. A custom function called get_rec_cosine() was developed which takes in
       the movie title (and year, if necessary) to generate a dataframe of recommended movies:
-        titles, similarity scores, and position in the IMDB top 1000."""),
+      titles, similarity scores, and position in the IMDB top 1000."""),
       
       ui.tags.br(),
       ui.h2("Diagnostics"),
@@ -105,12 +136,17 @@ app_ui = ui.page_fluid(
       ui.tags.br(),
       ui.h2("Keith Post"),
       ui.p(
-        ui.a("Repo", href="https://github.com/kpost34/imdb_ml_py"),
+        "For source code, click ", 
+        ui.a("here", href="https://github.com/kpost34/imdb_ml_py"),
         
         ui.tags.br(),
+        ui.tags.br(),
         
+        "Check out my ", 
         ui.a("GitHub", href="https://github.com/kpost34"),
+        " page for more coding projects",
         
+        ui.tags.br(),
         ui.tags.br(),
         
         ui.a("LinkedIn", href="https://www.linkedin.com/in/keith-post/")
@@ -125,27 +161,70 @@ app_ui = ui.page_fluid(
 # Server============================================================================================
 def server(input, output, session):
   @output
-  @render.data_frame
-  
-  def tab_recs():
+  @render.text
+  def txt_sel():
     movie_title = input.txt_movie()
-    n_recs = input.sld_n_recs()
     
-    #check if movie title is provided
+    if movie_title in df0['title'].values:
+      return "Selected Movie"
+    
+    else:
+      return ""
+    
+  @render.data_frame
+  def tab_sel():
+    movie_title = input.txt_movie()
+    
     if not movie_title:
       return pd.DataFrame()
     
-    #check if movie title is in database
     elif movie_title not in df0['title'].values:
       return pd.DataFrame({'Message': [f"""The movie '{movie_title}' is not in the database. Please 
                             enter a valid movie title."""]})
     
+    else: 
+      df_sel = get_movie_info(df=df0, title=movie_title)
+      
+      return df_sel
+  
+  @render.text
+  def txt_recs():
+    movie_title = input.txt_movie()
+    
+    if movie_title in df0['title'].values:
+      return "Recommended Movies"
+    
     else:
+      return ""
+    
+  @render.data_frame
+  def tab_recs():
+    movie_title = input.txt_movie()
+    n_recs = input.sld_n_recs()
+    sim_score = input.chk_sim_score()
+    imdb_rank = input.chk_imdb_rank()
+    
+    
+    #check if movie title is provided
+    # if not movie_title or movie_title not in df0['title'].values:
+    #   return pd.DataFrame()
+    
+    #check if movie title is in database
+    # elif movie_title not in df0['title'].values:
+    #   return pd.DataFrame({'Message': [f"""The movie '{movie_title}' is not in the database. Please 
+    #                         enter a valid movie title."""]})
+    
+    if movie_title in df0['title'].values:
       df_out = get_rec_cosine(df=df0, 
                               title=movie_title, 
                               mat=cosine_sim, 
-                              n=n_recs)
+                              n=n_recs,
+                              s_score=sim_score,
+                              i_rank=imdb_rank)
       return df_out
+    
+    else:
+      return pd.DataFrame()
 
 
 app = App(app_ui, server)
@@ -154,28 +233,12 @@ app = App(app_ui, server)
 #Updates:
 #1) rename the columns to say: "Movie Title", "Similarity Score", and "IMDB Rank"
 #2) add a row number field so that it ranks the recommendations from 1 to x
-#3) by default, it should return the title as well as the release year and genre (and maybe director
+#3) add functionality for checkboxes
+#4) by default, it should return the title as well as the release year and genre (and maybe director
   #and star1?)
-#4) add functionality for checkboxes
+
 #5) populate instructions tab
 #6) populate more info: diagnostics (once completed)
 #7) turn strings into objects that get imported
 #8) deploy app
-# 
-# import pandas as pd
-# 
-# x = range(5)
-# x = range(1, 6)
-# for n in x:
-#   print(n)
-# 
-# 
-#   
-#   
-#   
-# data = {'A': [10, 20, 30],
-#         'B': ["dog", "cat", "mouse"]}
-# data = pd.DataFrame(data)
-# 
-# 
-# data.insert(loc=0, column='Rank', value=range(1, len(data)+1))
+
